f7451a328dac3696cd607125616c1f30742320b6
cledmir <cledmirtejada@hotmail.com>
2020-09-27 01:05:01 -0500

    update bfs test

--- Testing Code/BFS_Test.py
+++ Testing Code/BFS_Test.py
@@ -1,9 +1,6 @@
 import math
 import pygame
-<<<<<<< HEAD
-=======
 import time
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
 # import networkx as nx
 
 class Tile():
@@ -15,11 +12,7 @@ class Tile():
 
 class Board():
     def __init__(self, size):
-<<<<<<< HEAD
-        self.board = [[Tile() for r in range(9)] for c in range(9)]
-=======
         self.board = [[Tile() for r in range(size)] for c in range(size)]
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
         self.indexing_tiles(self.board)
 
     def indexing_tiles(self, board):
@@ -47,20 +40,11 @@ class Board():
                 board[i][j].visited = False
                 board[i][j].visited_order = -1
 
-<<<<<<< HEAD
-class Player():
-=======
 class Player(pygame.sprite.Sprite):
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
     def __init__(self, color, xpos, ypos):
         self.color = color
         self.xpos = xpos
         self.ypos = ypos
-<<<<<<< HEAD
-
-class Game():
-    def __init__(self, num_players=4, size=9):
-=======
         super().__init__()
         self.image = pygame.image.load("meteor.png").convert()
         self.image.set_colorkey((0, 0, 0))
@@ -68,7 +52,6 @@ class Game():
 
 class Game():
     def __init__(self, num_players, size):
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
         self.players = []
         colors = ['red', 'blue', 'yellow', 'green']
         xpos = [int(size/2), int(size/2), 0, size-1]
@@ -78,15 +61,12 @@ class Game():
         for i in range(num_players):
             self.players.append(Player(colors[i], xpos[i], ypos[i]))
             self.game_board.board[ypos[i]][xpos[i]].element = self.players[i] 
-<<<<<<< HEAD
-=======
         self.all_sprite_list = pygame.sprite.Group()
     def printPlayer(self):
         for i in range(len(self.players)):
             self.players[i].rect.x = (self.players[i].xpos)*100
             self.players[i].rect.y = (self.players[i].ypos)*100
             self.all_sprite_list.add(self.players[i])
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
  
  #DFS
 def DFS(tile_ori, tile_dest, visited_order):
@@ -107,11 +87,7 @@ def call_DFS(game, pos_ori, pos_dest):
     game.game_board.print_visited_tiles(board_util)
     game.game_board.set_all_visited_false(board_util)
 
-<<<<<<< HEAD
-def BFS(pos_ori, pos_dest, game):
-=======
 def BFS(game, pos_ori, pos_dest):
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
     board_util = game.game_board.board
     tile_org = board_util[pos_ori[0]][pos_ori[1]]
     tile_dest = board_util[pos_dest[0]][pos_dest[1]]
@@ -119,27 +95,15 @@ def BFS(game, pos_ori, pos_dest):
     orden = 0
     queque.append(tile_org)
     while tile_org != tile_dest:
-<<<<<<< HEAD
-        tile_org = queque.pop(0)
-        tile_org.visited_order = orden
-        orden += 1
-
-=======
         
         tile_org = queque.pop(0)
         tile_org.visited_order = orden
         orden += 1
         tile_org.visited = True
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
         for i in tile_org.neighbours:
             if i.visited == False:
                 queque.append(i)
                 i.visited = True
-<<<<<<< HEAD
-    game.game_board.print_visited_tiles(board_util)
-    game.game_board.set_all_visited_false(board_util)
-
-=======
     
     
     game.game_board.print_visited_tiles(board_util)
@@ -151,30 +115,11 @@ def measure_time(sorting_alg, v):
   sorting_alg(v)
   end = time.time()
   return end-start
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
 
 def main():
     pygame.init()
     done = False
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-    SCREEN_WIDTH = 900
-    SCREEN_HEIGHT = 900
-    BLACK = (0,0,0)
-    WHITE = (255,255,255)
-    GREEN = (0,255,0)
-    RED = (255,0,0)
-    BLUE = (0,0,255)
-    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    game = Game()
-    pos_ori = [1,1]
-    pos_dest = [3,3]
-    BFS(pos_ori, pos_dest,game)
-=======
     n = 11
-=======
-    n = 5
->>>>>>> Stashed changes
     numPlayer = 4
     SCREEN_WIDTH = int((n)*100)
     SCREEN_HEIGHT = int((n)*100)
@@ -193,7 +138,6 @@ def main():
     call_DFS(game, pos_ori, pos_dest)
     end = time.time()
     print(end-start)
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
 
     while not done:
 
@@ -202,14 +146,6 @@ def main():
                 done = True
 
         screen.fill([255, 255, 255])
-<<<<<<< HEAD
-        for x in range(100,900,100):
-            pygame.draw.line(screen,BLACK, (x,0),(x,900), 2)
-        for y in range(100,900,100):
-            pygame.draw.line(screen,BLACK, (0,y),(900,y), 2)
-        pygame.display.flip()
-
-=======
         for x in range(100,SCREEN_WIDTH,100):
             pygame.draw.line(screen,BLACK, (x,0),(x,SCREEN_WIDTH), 2)
         for y in range(100,SCREEN_HEIGHT,100):
@@ -221,7 +157,6 @@ def main():
 
         
 
->>>>>>> 2b44ad5e98086ed4d707bef4ccca48c5243a2d91
     pygame.quit()
 
 
