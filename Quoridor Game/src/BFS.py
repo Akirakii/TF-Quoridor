aeeb4588bdec1d7af348fad41f05e862d2eb4d1a
cledmir <cledmirtejada@hotmail.com>
2020-09-29 01:46:40 -0500

    Update BFS.py

--- Quoridor Game/src/BFS.py
+++ Quoridor Game/src/BFS.py
@@ -1,8 +1,6 @@
 import Find_shortest_path as FSP
-def BFS(game, pos_ori, pos_dest):
-    board_util = game.game_board.board
-    tile_ori = board_util[pos_ori[0]][pos_ori[1]]
-    tile_dest = board_util[pos_dest[0]][pos_dest[1]]
+def BFS(board, pos_ori, goal):
+    tile_ori = board[pos_ori[0]][pos_ori[1]]
     queque = []
     order = 0
     queque.append(tile_ori)
@@ -13,13 +11,13 @@ def BFS(game, pos_ori, pos_dest):
         tile_ori.visited = True
         tile_ori.visited_order = order
         order += 1
-        if tile_ori == tile_dest:
+        if tile_ori in goal:
             break
         for i in tile_ori.neighbours:
             if queque.count(i) == 0 and i.visited == False:
                 queque.append(i)
     
-    FSP.find_shortest_path(tile_dest)
-    game.game_board.print_visited_tiles(board_util)
-    game.game_board.print_path(board_util)
-    game.game_board.reset_tiles(board_util)
+    FSP.find_shortest_path(tile_ori)
+    #game.game_board.print_visited_tiles(board_util)
+    #game.game_board.print_path(board_util)
+    #game.game_board.reset_tiles(board_util)
